29fdfec61058cf1c1fb46bdda10226f108d00b1f
NaomeJoyeuse <naomejoyce5@gmail.com>
2025-03-13 10:13:15 +0200

    update on oder

--- src/pages/academy/acad_orders.jsx
+++ src/pages/academy/acad_orders.jsx
@@ -214,7 +214,7 @@ const AcademyOrders = () => {
                           <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">#{order.id}</td>
                           <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{order.total_price || "N/A"}</td>
                           <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{formatDate(order.created_at)}</td>
-                          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">  {order.user}</td>
+                          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">  {order.user.role}</td>
                           <td className="px-6 py-4 whitespace-nowrap text-sm">
                             <span className={`px-2 py-1 inline-flex text-xs leading-5 font-semibold rounded-full 
                               ${order.status === 'completed' ? 'bg-green-100 text-green-800' : 
